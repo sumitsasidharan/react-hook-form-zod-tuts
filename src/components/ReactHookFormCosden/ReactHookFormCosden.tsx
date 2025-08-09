a570e8ad1d2b9e75c2150cc29e7d71e60673ac3f
sumitsasidharan <s.sumit196@gmail.com>
2025-08-09 21:12:47 +0530

    basics add

--- src/components/ReactHookFormCosden/ReactHookFormCosden.tsx
+++ src/components/ReactHookFormCosden/ReactHookFormCosden.tsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import { useForm, type SubmitHandler } from 'react-hook-form';
+
+type FormFields = {
+  email: string;
+  password: string;
+};
 
 const ReactHookFormCosden = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form action="">
-      <input type="text" name="" id="" placeholder='email' />
-      <input type="password" name="" id="" placeholder='Password' />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register('email', {
+          required: "Email is required",
+          // pattern: 'regex pattern'
+          validate: (value) => {
+            if (!value.includes("@")) {
+              return "Email must include @"
+            }
+            return true; // return true if input valid
+          },
+        })}
+        type="text"
+        placeholder="email"
+      />
+      {errors.email && <div>{errors.email.message}</div>}
+
+      <input
+        {...register('password', {
+          required: "Password is required",
+          // minLength: 8,
+          minLength: {
+            value: 8,
+            message: "Password must have at least 8 characters."
+          },
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      {errors.password && <div>{errors.password.message}</div>}
+
       <button type="submit">Submit</button>
     </form>
-  )
-}
+  );
+};
 
-export default ReactHookFormCosden
+export default ReactHookFormCosden;
